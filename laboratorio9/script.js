adb2fe5d7b95117d0c0f229fbdf79d885c0a17df
evangelistaaaron95 <evangelistaaaron95@alumnes.ilerna.com>
2026-01-21 22:08:39 +0100

    add laboratorio 9

---
+++ laboratorio9/script.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('btn-multi');
+
+btn.addEventListener('click', () => {
+    console.log("accion 1 ejecutada");
+});
+
+btn.addEventListener('click', () => {
+    alert("accion 2: Hola desde el segundo listener");
+});
+
+
+document.body.addEventListener('keydown', (e) => {
+    console.log(`Tipo: ${e.type}, Tecla: ${e.key}`);
+});
+
+const textarea = document.getElementById('campo-texto');
+
+textarea.addEventListener('keydown', (e) => {
+    if (e.key >= '0' && e.key <= '9') {
+        e.preventDefault(); 
+        alert("Error: No se permiten numeros en este campo.");
+    }
+});
